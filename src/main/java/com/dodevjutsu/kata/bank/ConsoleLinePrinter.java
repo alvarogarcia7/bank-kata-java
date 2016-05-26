dee17ad026eca34726f4fea808d1572c79579622
alvaro garcia <derecerca@gmail.com>
2016-05-26 21:59:07 +0200

    green

--- src/main/java/com/dodevjutsu/kata/bank/ConsoleLinePrinter.java
+++ src/main/java/com/dodevjutsu/kata/bank/ConsoleLinePrinter.java
@@ -1,12 +1,15 @@
 package com.dodevjutsu.kata.bank;
 
 public class ConsoleLinePrinter implements LinePrinter {
+    private final Console console;
+
     public ConsoleLinePrinter (final Console console) {
+        this.console = console;
     }
 
     @Override
     public void printHeader () {
-
+        console.print("date || credit || debit || balance");
     }
 
     @Override
