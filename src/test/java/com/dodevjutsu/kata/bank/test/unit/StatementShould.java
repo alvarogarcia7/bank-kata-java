3a6072e51687c4e6e121abbbc29eb4d99ce14dcb
alvaro garcia <derecerca@gmail.com>
2016-05-26 21:29:49 +0200

    refactor: whitespace

--- src/test/java/com/dodevjutsu/kata/bank/test/unit/StatementShould.java
+++ src/test/java/com/dodevjutsu/kata/bank/test/unit/StatementShould.java
@@ -27,10 +27,7 @@ public class StatementShould {
         Line line1 = new Line(new Date("10/01/2012"), new Amount(1000), new Balance(1000));
         Line line2 = new Line(new Date("13/01/2012"), new Amount(2000), new Balance(3000));
         Line line3 = new Line(new Date("14/01/2012"), new Amount(-500), new Balance(2500));
-        final Statement statement = new Statement(
-                line1,
-                line2,
-                line3);
+        final Statement statement = new Statement(line1, line2, line3);
 
         context.checking(new Expectations() {{
             oneOf(linePrinter).printHeader();
